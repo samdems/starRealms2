41597e99c03b2daa39a6a62a3d5ad3ff38ddc845
sam cross <sam.cross@gmx.com>
2024-04-28 11:55:54 +0100

    fix ts

--- src/stores/logStore.ts
+++ src/stores/logStore.ts
@@ -3,13 +3,15 @@ import { ref } from 'vue'
 
 export const useLogStore = defineStore('log', ()=> {
   
-  const logs = ref<{message:string,data:any,error:boolean}[]>([])
+  const logs = ref<{id:string,message:string,data:any,error:boolean}[]>([])
 
-  const log = (message:string,data:any) => {
-    logs.value.push({message, data, error:false})
+  const log = (message:string,data:any = '') => {
+    const id = Math.random().toString(36).substring(7)
+    logs.value.push({id, message, data, error:false})
   }
-  const logError = (message:string, data:any) => {
-    logs.value.push({message, data, error:true})
+  const logError = (message:string, data:any = '') => {
+    const id = Math.random().toString(36).substring(7)
+    logs.value.push({id,message, data, error:true})
   }
 
   return {
