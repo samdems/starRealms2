bc59ab7818a239c200b9c1fd8967c838857537ff
sam cross <sam.cross@gmx.com>
2024-04-28 09:58:21 +0100

    fix url render

--- src/components/GameSettings.vue
+++ src/components/GameSettings.vue
@@ -4,8 +4,8 @@
   <div class="modal-box">
     <div class="p-2 flex flex-col justify-center items-center">
       <p>Scan the QR code to join the game</p>
-      <qrcode-vue :value="value" level="H" class="m-2 p-2 bg-white" />
-      <p>{{value}}</p>
+      <qrcode-vue :value="value" size="200" level="H" class="m-4 p-2 bg-white" />
+      <p class="text-xs">{{value}}</p>
     </div>
     <div class="modal-action">
       <form method="dialog">
@@ -20,5 +20,5 @@
 <script setup lang="ts">
 import QrcodeVue from 'qrcode.vue';
 
-const value = window.location.href;
+const value = "https://star-realms.samcross.uk/#pqri9jw31" // window.location.href.replace('http://','');
 </script>
