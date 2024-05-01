8ebd9abc9ac4a74b6042438b3044fd93e319c1e5
sam cross <sam.cross@gmx.com>
2024-05-01 22:41:02 +0100

    move donat button to settings

--- src/components/kofi.vue
+++ src/components/kofi.vue
@@ -4,8 +4,8 @@
 <dialog id="kofiModal" class="modal">
   <div class="modal-box">
     <div class="p-2 flex flex-col justify-center items-center">
-      <h2 class="text-2xl">Ko-fi</h2> 
-      <iframe id='kofiframe' src='https://ko-fi.com/samdems/?hidefeed=true&widget=true&embed=true&preview=true' style='border:none;width:100%;padding:4px;background:#f9f9f9;' height='712' title='samdems'></iframe>
+      <iframe 
+        src='https://ko-fi.com/samdems/?hidefeed=true&widget=true' class="w-full rounded" height="650" title='samdems'></iframe>
     </div>
     <div class="modal-action">
       <form method="dialog">
